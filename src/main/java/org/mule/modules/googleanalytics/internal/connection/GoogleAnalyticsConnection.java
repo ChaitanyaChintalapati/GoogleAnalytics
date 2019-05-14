d51ca4cc0953a26cc15789d1a8b11f4ce2ba760c
chaitanya chintalapati <>
2019-05-14 10:28:13 -0500

    test fix

--- src/main/java/org/mule/modules/googleanalytics/internal/connection/GoogleAnalyticsConnection.java
+++ src/main/java/org/mule/modules/googleanalytics/internal/connection/GoogleAnalyticsConnection.java
@@ -1,6 +1,7 @@
 package org.mule.modules.googleanalytics.internal.connection;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
