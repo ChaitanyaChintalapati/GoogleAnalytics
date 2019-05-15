96c19c25164a70d30e5a9a7428227b8381e1e94f
chaitanya chintalapati <>
2019-05-14 22:23:55 -0500

    updated artifact id

--- src/main/java/org/mule/modules/googleanalytics/internal/connection/GoogleAnalyticsConnection.java
+++ src/main/java/org/mule/modules/googleanalytics/internal/connection/GoogleAnalyticsConnection.java
@@ -1,7 +1,6 @@
 package org.mule.modules.googleanalytics.internal.connection;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
